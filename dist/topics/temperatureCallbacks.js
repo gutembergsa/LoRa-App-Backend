8444b457ef05d5ae0b840fd4e4e6aa8382a268f7
Gutemberg Sá <gutemberg.silvasa@outlook.com>
2019-10-31 18:12:04 -0300

    heroku-mongodb

--- dist/topics/temperatureCallbacks.js
+++ dist/topics/temperatureCallbacks.js
@@ -22,12 +22,11 @@ exports.callbacks = {
     temperatureIncomingMessage: (topic, payload, packet) => {
         let nodePacket = new node_model_1.Node();
         let dateAux = new Date();
-        let pld = payload.toString().split('|');
+        let pld = payload.toString().split(' ');
         nodePacket.date = dateAux.getDate() + "-" + (dateAux.getMonth() + 1) + "-" + dateAux.getFullYear();
         nodePacket.hour = (dateAux.getHours() - 3) + ":" + dateAux.getMinutes();
         nodePacket.value = pld[0];
         nodePacket.latency = pld[1];
-        console.log(`${nodePacket}`);
         nodePacket.save().then(() => {
             console.log(`Pub salva: ${topic}`);
         });
--- topics/temperatureCallbacks.ts
+++ topics/temperatureCallbacks.ts
@@ -23,12 +23,11 @@ export const callbacks = {
     temperatureIncomingMessage:  (topic: string , payload:Buffer, packet:Packet) =>{
         let nodePacket = new Node()
         let dateAux = new Date()
-        let pld = payload.toString().split('|');
+        let pld = payload.toString().split(' ');
         nodePacket.date = dateAux.getDate() + "-" + (dateAux.getMonth() + 1) + "-" + dateAux.getFullYear();
         nodePacket.hour = (dateAux.getHours() - 3) + ":" + dateAux.getMinutes();
         nodePacket.value = pld[0]
         nodePacket.latency = pld[1]
-        console.log(`${nodePacket}`)
         nodePacket.save().then(()=>{
             console.log(`Pub salva: ${topic}`)
         })
