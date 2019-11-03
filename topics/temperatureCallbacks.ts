68025e8e191151c3237b6acbe87a89237f0e9fa5
Gutemberg Sá <gutemberg.silvasa@outlook.com>
2019-11-02 21:39:03 -0300

    heroku-mongodb

--- topics/temperatureCallbacks.ts
+++ topics/temperatureCallbacks.ts
@@ -25,7 +25,7 @@ export const callbacks = {
         let dateAux = new Date()
         let pld = payload.toString().split('|');
         nodePacket.date = `${dateAux.getHours() > 20 ? (dateAux.getDate() - 1) : dateAux.getDate()}-${(dateAux.getMonth() + 1)}-${dateAux.getFullYear()}`;
-        nodePacket.hour = `${dateAux.getHours() > 20 ? dateAux.getHours() : (dateAux.getHours() - 3)}:${dateAux.getMinutes()}`;
+        nodePacket.hour = `${dateAux.getHours() > 20 ? dateAux.getHours() + 21 : (dateAux.getHours() - 3)}:${dateAux.getMinutes()}`;
         nodePacket.value = pld[0]
         nodePacket.latency = pld[1]
         console.log(`packet: ${nodePacket}`)
