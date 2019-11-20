2718c8303835464fe5062da9b6b75e2b82ebeb13
Gutemberg Sá <gutemberg.silvasa@outlook.com>
2019-11-19 21:00:49 -0300

    heroku-mongodb

--- dist/topics/callbacks/statusCallbacks.js
+++ dist/topics/callbacks/statusCallbacks.js
@@ -22,10 +22,9 @@ exports.statusCallbacks = {
     },
     StatusIncomingMessage: (topic, payload, _packet) => {
         if (topic === 'ratings') {
-            console.log(`payload: ${payload.toString()}`);
             let pld = payload.toString().split('|');
             let nodePacket = new status_model_1.statusCollection();
-            [nodePacket.receive, nodePacket.sent] = [pld[0], pld[1]];
+            [nodePacket.sent, nodePacket.receive] = [pld[0], pld[1]];
             console.log(`packet: ${nodePacket}`);
             nodePacket.save().then(() => {
                 console.log(`Pub salva: ${topic}`);
--- topics/callbacks/statusCallbacks.ts
+++ topics/callbacks/statusCallbacks.ts
@@ -22,10 +22,9 @@ export const statusCallbacks = {
     },
     StatusIncomingMessage:(topic: string, payload:Buffer, _packet:Packet) =>{
         if (topic === 'ratings') {
-            console.log(`payload: ${payload.toString()}`)
             let pld = payload.toString().split('|')
             let nodePacket = new statusCollection();
-            [nodePacket.receive, nodePacket.sent] = [pld[0], pld[1]]
+            [nodePacket.sent, nodePacket.receive] = [pld[0], pld[1]]
             console.log(`packet: ${nodePacket}`)
             nodePacket.save().then(()=>{
                 console.log(`Pub salva: ${topic}`)
